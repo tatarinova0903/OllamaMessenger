5c7ea6a8a41322a13a56fb3b65856b38b559be50
tatarinova999 <tatarinova999@yandex-team.ru>
2025-02-23 12:26:02 +0300

    create context for ollama

--- Targets/OllamaMessenger/Sources/Services/OllamaService.swift
+++ Targets/OllamaMessenger/Sources/Services/OllamaService.swift
@@ -2,16 +2,18 @@ import Ollama
 
 @MainActor
 final class OllamaService {
+    private static let llama3_2: Model.ID = "llama3.2"
+
     private let client = Client.default
-    private let model: Model.ID = "llama3.2"
+    private var context = [Chat.Message]()
 
     func sendMessage(msg: String) async throws -> String {
+        context.append(.user(msg))
         let response = try await client.chat(
-            model: model,
-            messages: [
-                .user(msg)
-            ]
+            model: Self.llama3_2,
+            messages: context
         )
+        context.append(response.message)
         return response.message.content
     }
 }
